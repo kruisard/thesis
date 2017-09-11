0ebc28aa413c00972ccddc8dc49728d2e9037d0a
kruisard <kierstenjean@gmail.com>
2017-09-11 22:30:21 +0100

    re-org chapters re:tim input

--- pdftx.sh
+++ pdftx.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # need to run dos2unix after editting
 # run pdf latex with bibtex
-pdflatex $1.tex &&
-bibtex $1 &&
-pdflatex $1.tex &&
-pdflatex $1.tex &&
-cmd /c start SumatraPDF $1.pdf
+pdflatex $1.main.tex &&
+bibtex $1.main &&
+pdflatex $1.main.tex &&
+pdflatex $1.main.tex &&
+cmd /c start SumatraPDF $1.main.pdf
 
-rm $1.log $1.aux $1.bbl $1.blg $1.ps $1.out $1.dvi
+rm $1.main.log $1.main.aux $1.main.bbl $1.main.blg $1.main.ps $1.main.out $1.main.dvi
+rm $1.Chapter.aux $1.Chapter.log
