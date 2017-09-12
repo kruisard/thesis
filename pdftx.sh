8fc4da78bc11f241f5851250a96e2e84b2efeee4
kruisard <kierstenjean@gmail.com>
2017-09-12 16:28:18 +0100

    finished re-org of figures files

--- pdftx.sh
+++ pdftx.sh
@@ -7,5 +7,4 @@ pdflatex $1.main.tex &&
 pdflatex $1.main.tex &&
 cmd /c start SumatraPDF $1.main.pdf
 
-rm $1.main.log $1.main.aux $1.main.bbl $1.main.blg $1.main.ps $1.main.out $1.main.dvi
-rm $1.Chapter.aux $1.Chapter.log
+rm *.log *.aux *.bbl *.blg *.ps *.out *.dvi
